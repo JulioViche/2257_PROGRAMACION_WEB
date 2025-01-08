2d33e437d724fe5148ea8b4ca9bfb04f69fabbe9
Julio Viche <jeviche@espe.edu.ec>
2025-01-08 10:46:46 -0500

    Actividad 2

---
+++ 2P/250108_ACTIVIDAD_2/actividad2.js
@@ -0,0 +1,45 @@
+function Producto (nombre, precio, cantidad) {
+    this.nombre = nombre;
+    this.precio = (precio > 0) ? precio : 0;
+    this.cantidad = cantidad;
+}
+
+const inventario = {
+    productos: [
+        new Producto('Pasta dental', 2.50, 22),
+        new Producto('Cepillo dental', 1.50, 12),
+        new Producto('Shampoo', 10.50, 20),
+        new Producto('Papel higiénico', 1.00, 32),
+    ],
+    vender: function (nombreProducto, cantidad) {
+        let productoSeleccionado;
+        for (producto of this.productos)
+            if (nombreProducto === producto.nombre)
+                productoSeleccionado = producto;
+
+        if (!productoSeleccionado) {
+            console.log('El producto no existe.');
+            return;
+        }
+
+        if (cantidad > productoSeleccionado.cantidad) {
+            console.log('No hay tanta cantidad en stock.');
+            return;
+        }
+
+        productoSeleccionado.cantidad -= cantidad;
+        console.log(`Se vendieron ${cantidad} unidades de ${productoSeleccionado.nombre}. Valor: $${cantidad * productoSeleccionado.precio}`);
+    },
+    aplicarDescuento: function (descuento) {
+        if (descuento <= 0) {
+            console.log('No se pudo aplicar el descuento.');
+            return;
+        }
+
+        for (producto in this.productos) {
+            producto.precio *= 1 - descuento;
+        }
+    }
+}
+
+inventario.vender('Shampoo', 421);
---
+++ 2P/250108_JS_OBJETOS/objetos.js
@@ -0,0 +1,123 @@
+/*
+    OBJETOS
+  - Claves y Valores
+  - Dinamismo
+  - Propiedades y Métodos
+  - Declaración de objetos
+*/
+
+const persona = {
+    nombre: 'Juan',
+    apellido: 'Pérez',
+    email: 'juanpérez@mail.com',
+    edad: 25
+}
+
+console.log(persona);
+
+// MÉTODOS
+
+const persona2 = {
+    nombre: 'Darío',
+    apellido: 'Vargas',
+    direccion: 'Quito',
+    email: 'dvargas@mail.com',
+
+    nombreCompleto: function () {
+        return this.nombre + ' ' + this.apellido;
+    }
+}
+
+console.log(persona2.nombreCompleto());
+
+const persona3 = {
+    nombre: 'Maria',
+    edad: 25,
+    saludar: function () {
+        console.log(`Hola, me llamo ${this.nombre}`)
+    }
+}
+
+persona3.saludar();
+
+// Constructor de Object
+
+const persona4 = new Object();
+persona4.nombre = 'José';
+persona4.apellido = 'Martínez';
+persona4.saludar = function () {
+    console.log(`Hola, me llamo ${this.nombre}`);
+}
+
+persona4.saludar();
+
+// Prototipo con Object.create
+
+const prototipoPersona = {
+    saludar: function () {
+        console.log(`Hola desde prototipo, soy ${this.nombre}`)
+    }
+}
+
+const persona5 = Object.create(prototipoPersona);
+persona5.nombre = 'Carlos';
+persona5.saludar();
+
+//  Clases
+
+class Persona {
+    constructor(nombre, apellido, edad, telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.telefono = telefono;
+    }
+
+    saludar() {
+        console.log(`Hola desde clase, soy ${this.nombre}`);
+    }
+}
+
+let persona6 = new Persona('Ana', 'Rios', 24, '0990372534');
+persona6.saludar();
+
+// Funciones Constructoras (Clásica)
+
+function Persona1 (nombre, apellido, edad, telefono) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+    this.telefono = telefono;
+    this.saludar = function () {
+        console.log(`Hola desde función constructor, soy ${this.nombre}`);
+    }
+}
+
+let persona7 = new Persona('Luis', 'Gonzáles', 21, '0990372534');
+persona7.saludar();
+
+// Modificación, eliminación y adición de atributos
+
+const obj = {nombre: 'Objeto1'};
+obj.tipo = 'utensilio';
+obj.material = 'papel';
+
+delete obj.nombre;
+
+console.log(obj);
+
+// Manipular datos
+
+for (clave in obj) {
+    console.log(`${clave}: ${obj[clave]}`);
+}
+
+console.log('nombre' in obj);
+console.log(obj.hasOwnProperty('material'));
+console.log(Object.keys(obj));
+console.log(Object.values(obj));
+console.log(Object.entries(obj));
+
+Object.seal(obj);
+let objString = JSON.stringify(obj);
+console.log(objString);
